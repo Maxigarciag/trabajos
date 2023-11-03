23b8a2ef082553eee8717ca497b7c48fe6ea62d5
Maxigarciag <maximogarciag19@gmail.com>
2023-11-02 21:20:38 -0300

    login
    ..

---
+++ main.js
@@ -0,0 +1,56 @@
+function login() 
+{
+    var name = document.getElementById("email").value;
+    var password = document.getElementById("contraseña").value;
+
+    if (name === "maximo" && password === "1234") {
+        window.location = "page.html";
+    } else {
+        alert("Usuario incorrecto");
+    }
+}
+
+
+$(document).ready(function () {
+    "use strict";
+    
+   
+
+    // Dropdown on mouse hover
+    function toggleNavbarMethod() {
+        if ($(window).width() > 992) {
+            $('.navbar .dropdown').on('mouseover', function () {
+                $('.dropdown-toggle', this).trigger('click');
+            }).on('mouseout', function () {
+                $('.dropdown-toggle', this).trigger('click').blur();
+            });
+        } else {
+            $('.navbar .dropdown').off('mouseover').off('mouseout');
+        }
+    }
+    toggleNavbarMethod();
+    $(window).resize(toggleNavbarMethod);
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+});
+
